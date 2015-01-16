c5694ce74aa8eba2f6828ddf17ec9082189f922b
Phil Evang <phil.evang@gmail.com>
2015-01-17 00:36:11 +0100

    clear up styling and markup; add init to js;

--- busbike.js
+++ busbike.js
@@ -1,25 +1,51 @@
-window.onload=loadWeather;
+window.onload=init;
 
-function auswahlCity(url){
+function init() {
     var ibk=document.getElementById("ibk");
     var sbg=document.getElementById("sbg");
     var vn=document.getElementById("vn");
-    
-    if (ibk.onclick){
-        url="http://api.openweathermap.org/data/2.5/weather?q=Innsbruck,at&units=metric"
-    }
-    if (sbg.onclick){
-        url="http://api.openweathermap.org/data/2.5/weather?q=Salzburg,at&units=metric"
-    }
-    if (vn.onclick){
-        url="http://api.openweathermap.org/data/2.5/weather?q=Wien,at&units=metric"
-    }
+
+    ibk.addEventListener("click",auswahlCity,false);
+    sbg.addEventListener("click",auswahlCity,false);
+    vn.addEventListener("click",auswahlCity,false);
+
+    loadWeather("Innsbruck,at");
 }
 
-auswahlCity();
+function auswahlCity(){
+    // if (ibk.onclick){
+    //     url="http://api.openweathermap.org/data/2.5/weather?q=Innsbruck,at&units=metric"
+    // }
+    // if (sbg.onclick){
+    //     url="http://api.openweathermap.org/data/2.5/weather?q=Salzburg,at&units=metric"
+    // }
+    // if (vn.onclick){
+    //     url="http://api.openweathermap.org/data/2.5/weather?q=Wien,at&units=metric"
+    // }
 
-function loadWeather(url){
-    //var url = "http://api.openweathermap.org/data/2.5/weather?q=Innsbruck,at&units=metric";
+    // class active wird zuerst bei allen buttons gelöscht und dann bei angeklicktem angehängt
+    for(var i in this.parentNode.children){
+        this.parentNode.children[i].className = 'button';
+    }
+    this.className += ' active';
+
+    var city = "";
+    if(this.id == 'ibk'){
+        city = "Innsbruck,at";
+    }
+    else if(this.id == 'sbg'){
+        city = "Salzburg,at";
+    }
+    else{
+        city = "Vienna,at";
+    }
+
+    loadWeather(city);
+}
+
+
+function loadWeather(city){
+    var url = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric";
     
     
     var request = new XMLHttpRequest();
